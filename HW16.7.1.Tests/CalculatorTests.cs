5b617f7a36449cc811a31cf4eae259d42d2d365c
Valentisa11 <valya.kobylina@yandex.ru>
2023-02-15 16:33:21 +0300

    Добавьте файлы проекта.

---
+++ HW16.7.1.Tests/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace HW16._7._1.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(20, 10) == 30);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(200, 10) == 2000);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+
+        }
+
+    }
+}
---
+++ HW16.7.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW16._7._1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
+     
